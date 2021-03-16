4bfe9d2d169ed35b92ab320c8695852752b5a18c
zyzzyxis <13ezimm@gmail.com>
2021-03-16 16:30:18 -0600

    added button demo and styles

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,19 @@ import {Switch, Route} from 'react-router-dom'
 import {Container} from 'semantic-ui-react'
 import NavBar from './components/NavBar';
 import About from './pages/About';
+import { PRIMARY_COLOR } from './styles';
+import ComponentDemo from './pages/ComponentDemo'
 
 function App() {
   return (
     <>
     <NavBar />
+    <h1 style={{color: PRIMARY_COLOR}}>Page Heading</h1>
     <Container>
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/about' component={About}/>
+      <Route exact path='/componentDemo' component={ComponentDemo}/>
     </Switch>
     </Container>
     </>
